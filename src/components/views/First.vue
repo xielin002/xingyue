a6b3e181a577398eb57a74a874f0b6f07966c57d
xielin002 <463155738@qq.com>
2020-10-05 16:30:33 +0800

    lin

---
+++ src/components/views/First.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="first-app">
+    {{msg}}
+    <confirm text="注册" @message="getMessage"/>
+  </div>
+    
+</template>
+
+<script>
+import Confirm from '../sub/Comfirm'
+export default {
+  name: 'First',
+  components:{
+      Confirm
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your First App'
+    }
+  },
+  methods:{
+      getMessage(val){
+        alert(val);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
